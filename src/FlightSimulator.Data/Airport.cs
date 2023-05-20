e33a3d5e825794194d3a691f9a6c2805eda9eb88
Eamonn Watson <Eamonn.Watson@flyacva.com>
2023-05-19 22:32:39 -0300

    Added Data Types

---
+++ src/FlightSimulator.Data/Airport.cs
@@ -0,0 +1,42 @@
+﻿using FlightSimulator.Data.Units;
+
+namespace FlightSimulator.Data;
+
+public sealed class Airport
+{
+    /// <summary>
+    /// ICAO name of the airport
+    /// </summary>
+    public string ICAO { get; internal set; }
+
+    /// <summary>
+    /// Name of the airport as given in Flight Simulator
+    /// </summary>
+    public string AirportName { get; internal set; }
+
+    /// <summary>
+    /// Latitude/Longtitude of airport
+    /// </summary>
+    public GeoPosition Position { get; internal set; }
+
+    /// <summary>
+    /// Country of airport
+    /// </summary>
+    public string Country { get; internal set; }
+
+    /// <summary>
+    /// State of Airport
+    /// </summary>
+    public string State { get; internal set; }
+
+    /// <summary>
+    /// City of airport
+    /// </summary>
+    public string City { get; internal set; }
+
+    /// <summary>
+    /// Altitude of airport
+    /// </summary>
+    public Distance Altitude { get; internal set; }
+
+}
---
+++ src/FlightSimulator.Data/FlightSimData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Data;
+public sealed class FlightSimData
+{
+    public DateTime DataDateTime { get; internal set; }
+    public SimulatorData Simulator { get; internal set; }
+    public AircraftData Aircraft { get; internal set; }
+    public Airport NearestAirport { get; internal set; }
+    public WeatherData Weather { get; internal set; }
+    public long WatchDog { get; internal set; }
+
+    internal FlightSimData() { }
+}
